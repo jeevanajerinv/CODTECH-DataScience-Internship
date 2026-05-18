08c56d2ec7255c95ecfef046db41700fbdef5aad
jeevanajerinv <jeevanajerinv@gmail.com>
2026-05-18 12:23:09 +0530

    Added Task 3 End-to-End Data Science Project

---
+++ Task3_EndToEndProject/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load("house_price_model.pkl")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [
+        float(request.form['MedInc']),
+        float(request.form['HouseAge']),
+        float(request.form['AveRooms']),
+        float(request.form['AveBedrms']),
+        float(request.form['Population']),
+        float(request.form['AveOccup']),
+        float(request.form['Latitude']),
+        float(request.form['Longitude'])
+    ]
+
+    prediction = model.predict([features])[0]
+
+    return render_template(
+        'index.html',
+        prediction_text=f'Predicted House Price Value: {prediction:.2f}'
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
